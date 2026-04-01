2b805d934d4f1f91a7efdde7f614971d9934a66d
SandipDeb05 <sandipdeb05@gmail.com>
2026-04-02 00:25:41 +0530

    learned java oops'

---
+++ src/oops/encapsulation/Root.java
@@ -0,0 +1,24 @@
+package encapsulation;
+
+public class Root {
+    public static void main(String[] args) {
+        Laptop lenovo = new Laptop();
+        lenovo.setPrice(200);
+        System.out.println(lenovo.getPrice());
+    }
+}
+
+class Laptop {
+    int ram;
+    private int price;
+    boolean isAdmin;
+
+    public void setPrice(int price) {
+        if(isAdmin) {
+            this.price = price;
+        }
+    }
+    int getPrice() {
+        return this.price;
+    }
+}
